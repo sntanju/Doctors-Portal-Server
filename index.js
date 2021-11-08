5dfbcbf49a0c01ebe6389bf716541d5f51e9d231
sntanju <sntanju07@gmail.com>
2021-11-08 22:36:22 +0600

    heroku-error-4

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
+console.log(serviceAccount)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -20,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k5fnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
